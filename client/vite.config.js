75dbcfe420aed4f2798f22db38638b9d92dc2d4d
Mary Ann Jose <maryannjose129@gmail.com>
2024-06-30 20:44:09 +0530

    please work aaku

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'https://bubblebrew-server-latest.onrender.com'
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://bubblebrew-server-latest.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   },
   plugins: [react()],
