1ded797c952de84f3ce4228d079b8740bd28d535
Mary Ann Jose <maryannjose129@gmail.com>
2024-06-30 20:53:41 +0530

    manndsnds

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   server: {
@@ -7,9 +7,9 @@ export default defineConfig({
       '/api': {
         target: 'https://bubblebrew-server-latest.onrender.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
   plugins: [react()],
-})
+});
