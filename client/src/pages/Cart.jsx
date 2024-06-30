4304631a747cab1725db734d179506a697206665
Mary Ann Jose <maryannjose129@gmail.com>
2024-06-30 23:14:36 +0530

    MEOW

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Cartitem from "../components/Cartitem";
 import axios from 'axios';
 import { ToastContainer, toast } from "react-toastify";
